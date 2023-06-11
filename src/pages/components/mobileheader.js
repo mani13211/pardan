ba6c9829776537084dc70281779b169bfda1740e
mani13211 <58296779+mani13211@users.noreply.github.com>
2023-06-11 22:32:37 +0530

    push

--- src/pages/components/mobileheader.js
+++ src/pages/components/mobileheader.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { ImPencil2 } from 'react-icons/im';
+import { BsInstagram } from 'react-icons/bs';
+import Link from 'next/link';
 
 function Mobileheader() {
   return (
@@ -7,6 +9,7 @@ function Mobileheader() {
          <div className='logo'>
                 <ImPencil2 className='me-2'/>
                 <i className="bi bi-palette"></i><span>RKDraws</span>
+                {/* <Link className="insta_a" href="https://www.instagram.com/smile.draws/"><BsInstagram /></Link> */}
            </div>
     </div>
   )
