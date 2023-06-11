df7121beb06105a1fdbb1ce7ec981702af87cde1
mani13211 <58296779+mani13211@users.noreply.github.com>
2023-06-11 20:06:33 +0530

    pricing done

--- src/pages/components/navbar.js
+++ src/pages/components/navbar.js
@@ -1,14 +1,15 @@
 import React from 'react'
+import Link from 'next/link'
 
 function Navbar() {
   return (
     
     <div className='navbar'>
         <ul>
-            <li>Portifolio</li>
+         <Link href="/">   <li>Portifolio</li></Link>
             <li>About ME</li>
-            <li>Pricing</li>
-            <li>Contact US</li>
+            <Link href="pricing">  <li>Pricing</li></Link>
+            <Link href="contact">  <li>Contact US</li></Link>
         </ul>
          </div>
   )
