05fa4fc0432e6c2ef149a7433e1a47e2cc11eac3
mani13211 <58296779+mani13211@users.noreply.github.com>
2023-06-11 15:10:41 +0530

    firspush gallry setup

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,9 @@
-import '@/styles/globals.css'
+import '@/styles/css/globals.css'
+import Sidebar from './components/sidebar'
+import "../../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import Navbar from './components/navbar'
+//import "../../node_modules/bootstrap/dist/js/bootstrap"
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <><Sidebar/><Component {...pageProps} /></>
 }
---
+++ src/pages/components/navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    
+    <div className='navbar'>
+        <ul>
+            <li>Portifolio</li>
+            <li>About ME</li>
+            <li>Pricing</li>
+            <li>Contact US</li>
+        </ul>
+         </div>
+  )
+}
+
+export default Navbar
