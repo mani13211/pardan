f6a47af9281ad188095f1ae0495766786674b09b
mani13211 <58296779+mani13211@users.noreply.github.com>
2023-06-13 15:27:50 +0530

    cnges

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -4,7 +4,12 @@ const connectDb = handler =>async(req,res)=>{
   if(mongoose.connections[0].readyState){
     return handler(req,res)
   }
-  await mongoose.connect(process.env.MONGO_URI)
+  await mongoose.connect(process.env.MONGO_URI, {
+
+    useNewUrlParser: "true",
+    useUnifiedTopology: "true"
+  
+  })
   return handler(req,res)
 }
 export default connectDb
