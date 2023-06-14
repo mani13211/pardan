346ca17e5f81fb1cc437ec7ca05e05d9c409a6db
mani13211 <58296779+mani13211@users.noreply.github.com>
2023-06-14 17:46:56 +0530

    imagweas

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,16 @@ import { Main } from 'next/document'
 import Head from 'next/head'
 import { useState } from 'react'
 import Mobileheader from './components/mobileheader';
-import Collection from '../../models.js/Collection';
+//import Collection from '../../models.js/Collection'
+import path from "path";
 import {ImCross} from 'react-icons/im';
 import Image from 'next/image';
+import fs from "fs/promises"
 import mongoose from 'mongoose';
 import Navbar from './components/navbar'
 
 
-const Home=({data})=> {
+const Home=({dirs})=> {
 
     
 const [model, setmodel] = useState(false)
@@ -30,7 +32,7 @@ const getImage=(imgscr)=>{
             <Mobileheader/>
             <Navbar/>
             <div className='gallary mt-3'>
-             
+{/*              
               {data.map((i,index)=>{
                 return(
                   <div className='pics' key={index} onClick={()=>getImage(i.img_path)}>
@@ -43,7 +45,19 @@ const getImage=(imgscr)=>{
               )
               
                 
-              })}
+              })} */}
+               {dirs.map(item => (
+
+                  <div className='pics' key={item} onClick={()=>getImage("/images/" + item)}>
+                  <Image src={"/images/" + item} width={0}
+                  height={0}
+                  sizes="100vw"
+                  style={{ width: '100%', height: 'auto' }}    alt= "" />
+                  </div>
+                            
+        
+        ))}
+
             </div>
             
           </div>
@@ -54,12 +68,27 @@ const getImage=(imgscr)=>{
     </>
   )
 }
-export async function getServerSideProps(context) {
-  if(! mongoose.connections[0].readyState){
-    await mongoose.connect("mongodb+srv://paymentsetup:9815897261@cluster0.xayu3wr.mongodb.net/")
-  }
-  const collection = await Collection.find({})
+// export async function getServerSideProps(context) {
+//   if(! mongoose.connections[0].readyState){
+//     await mongoose.connect("mongodb+srv://paymentsetup:9815897261@cluster0.xayu3wr.mongodb.net/")
+//   }
+//   const collection = await Collection.find({})
  
-  return { props:{data:JSON.parse(JSON.stringify(collection)) }}
+//   return { props:{data:JSON.parse(JSON.stringify(collection)) }}
+// }
+
+
+export const getServerSideProps = async (context) => {
+
+  const props = { dirs: [] }
+  try {
+    const dirs = await fs.readdir(path.join(process.cwd(), "/public/images"))
+    console.log(dirs,"i m dirs")
+    props.dirs = dirs
+    return { props }
+  } catch (error) {
+    return { props }
+  }
 }
+ 
 export default  Home
