431fea322b8b0de4798faf1c865f1a4b0279e3fe
mani13211 <58296779+mani13211@users.noreply.github.com>
2023-06-17 21:55:57 +0530

    chngres

--- src/pages/admin.js
+++ src/pages/admin.js
@@ -4,11 +4,12 @@ import Navbar from './components/navbar'
 import router, { useRouter } from 'next/router';
 import Image from 'next/image'
 import Link from 'next/link';
-import {RxCross2}  from 'react-icons/rx'
+import { BiDotsVerticalRounded } from 'react-icons/bi'
 import mongoose from 'mongoose';
 //import Collection from '../../models.js/Collection';
-import { useState } from 'react'
-import { GiHand } from 'react-icons/gi';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import { useState } from 'react';
+import { GiConsoleController, GiHand } from 'react-icons/gi';
 
 import { IoMdPower } from 'react-icons/io';
 function Admin({ images }) {
@@ -24,9 +25,12 @@ function Admin({ images }) {
   }
   const [image, setimage] = useState("")
   const [uploader, setuploader] = useState(null)
+  const [model, setmodel] = useState("close")
+  const [delimg, setdelimg] = useState("")
+  const [imgsrr, setimgsrc] = useState("")
   const router = useRouter();
 
-  const handleSubmit =async(e)=>{
+  const handleSubmit = async (e) => {
     e.preventDefault()
     setuploader("1")
     const data = new FormData();
@@ -37,65 +41,102 @@ function Admin({ images }) {
         method: "POST", // or 'PUT'
         body: data,
       });
-      
+
       const result = await response.json();
-      
+
       console.log("Success:", result);
     } catch (error) {
       console.error("Error:", error);
     }
     setuploader(null)
     router.reload()
-    }
+  }
 
-    const deleteImg=async(id)=>{
-        const response = await fetch(`${process.env.NEXT_PUBLIC_HOST}/api/delete`, {
-          method: "POST", // or 'PUT'
-          body: JSON.stringify(id),
-        });
-        const result=await response.json()
-      
-        console.log(result.message)
-        router.reload()
-    }
+  const deleteImg = async (id) => {
+    console.log(id)
+    const response = await fetch(`${process.env.NEXT_PUBLIC_HOST}/api/delete`, {
+      method: "POST", // or 'PUT'
+      body: JSON.stringify(id),
+    });
+    const result = await response.json()
+
+    console.log(result.message)
+    router.reload()
+  }
+
+
   return (
+
+
+
+
     <div className='main '>
+
+
+     
+
+
       <Mobileheader />
       <Navbar />
+
       <div className='container admin-container  mt-3'>
         <div className='flex d-flex w-100  justify-content-between '><div><h5> <GiHand className='me-2' />Hi Dear</h5></div><div><p onClick={logoutUser}> <IoMdPower className='me-2' />Logout</p></div> </div>
 
         <div className='admin-cards-galary container-sm'>
           <h3>Upload new Image</h3>
 
-          <form onSubmit={handleSubmit}>
+          <form onSubmit={handleSubmit} className=' mb-3 mt-3 d-flex align-items-center'>
             <input type="file" onChange={(e) => {
               setimage(e.target.files[0])
             }} />
-            {image &&    (uploader ? <button className='btn btn-danger ' disabled>Uploading</button>: <button className='btn btn-danger '>Upload</button> ) } 
-           
-           
-           
+            {image && (uploader ? <button className='btn btn-danger ' disabled>Uploading</button> : <button className='btn btn-danger '>Upload</button>)}
           </form>
-
-
           <h3>MY Uploads</h3>
-          <div className='gallary mt-3'>
+          <div className='gallaryad row  mt-3'>
             {images.map((i, key) => {
-              return <div key={key} className='pics'  >
-                <Image src={i.image} width={0}
-                  height={0}
-                  sizes="100vw"
-                  style={{ width: '100%', height: '100%' }} alt="" />
-                  <RxCross2  onClick={() => {deleteImg(i.title)}} className='icon'/>
-                  
+              return <div key={key} className=' pics col-6 col-sm-6 col-lg-4 col-xl-3 '  >
+                <div className='card'>
+                  <Image src={i.image} width={0}
+                    height={0}
+                    sizes="100vw"
+                    style={{ width: '100%', height: '100%' }} alt="" />
+
+                  {/* <button className='btn btn-danger'>Delete</button> */}
+                  <div class="dropdown">
+                    <BiDotsVerticalRounded className='icon' type="button" data-bs-toggle="dropdown" aria-expanded="false" />
+                    <ul class="dropdown-menu">
+
+                      <li><a class="dropdown-item" onClick={ (()=>{
+                        
+                        setmodel("open")
+                          setimgsrc( i.image)
+                        setdelimg(i.title)
+
+                      }) }>Delete</a></li>
+                    </ul>
+                  </div>
+
+
+                </div>
               </div>
             })}
           </div>
-
+        </div>
+      </div>
+      <div className={`modeladmin ${model}`}>
+        <div className='modal-context'>
+          <div className='header'>Delete this picture</div>
+          <div className='body'><img src={imgsrr} /></div>
+          <div className='footer'> <button className='btn btn-danger ms-2 me-2' onClick={(()=>{
+            console.log(delimg)
+            deleteImg(delimg)
+          })} >Delete</button>  <button className='btn btn-secondary ms-2 me-2' onClick={(() => { setmodel("close") })} >Cancel</button></div>
         </div>
       </div>
     </div>
+
+
+
   )
 }
 
